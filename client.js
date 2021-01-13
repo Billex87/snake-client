ecb5d7cf01caccc7a68668564b3915c9578195cc
Billy <billy_mckinnon@hotmail.com>
2021-01-13 20:37:57 +0000

    Final revision

--- client.js
+++ client.js
@@ -1,4 +1,17 @@
-play.js
-const { connect } = require('./client');
-console.log('Connecting ...');
-connect();
+const net = require('net');
+const { HOST, PORT, NAME } = require('./constants')
+const connect = function () {
+  const conn = net.createConnection({
+    host: HOST,
+    port: PORT
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log('Intruder Alert: You Are Connected');
+    conn.write(`${NAME}`);
+  });
+
+  return conn;
+}
+module.exports = { connect };
