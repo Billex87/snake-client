5180eb317ca7106bc71e355de06e53bd0eed6b07
Billy <billy_mckinnon@hotmail.com>
2021-01-13 20:37:52 +0000

    Final revision

--- play.js
+++ play.js
@@ -1,40 +1,7 @@
-const net = require('net');
-const {HOST, PORT, NAME} = require('./constants')
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-const handleUserInput = (key) => {
-  if(key === '\u0003') {
-    process.exit
-  }
-
-}
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
-
-
-
-
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: HOST,
-    port: PORT
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('connect', () => {
-    console.log('Connected');
-    conn.write(`${NAME}`);
-   
-
-
-  });
-
-  return conn;
-}
 console.log('Connecting ...');
-let conn = connect();
-setupInput(conn);
+// connect();
+
+setupInput(connect());
